282725db282efe2fe72467688e10c3ddbeea3066
jim wen <jim.wen@monash.edu>
2025-09-22 11:51:12 +1000

    Adjust layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans items-center justify-items-center pt-8">
       <div className="bg-white rounded-lg pt-56 shadow-lg w-4/5 h-[800px] border-l-4 border-primaryLight items-center  justify-center justify-items-center">
-        <p><b>Reserve Zone</b></p>
+        <p className="text-7xl"><b>Reserve Zone</b></p>
       </div>
       <footer className="row-start-3 flex gap-[24px] pt-8 flex-wrap items-center justify-center">
         All rights reserved @2025
--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -22,8 +22,8 @@ export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
 
   const USER = {
-    name: "John Smith",
-    email: "johnson@nextadmin.com",
+    name: "Jim",
+    email: "jim.wen@monash.edu",
   };
 
   return (
